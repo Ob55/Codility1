662a0fea0a8f3b32d669ada66283178e1eae15c3
Ob55 <brian55mwangi@gmail.com>
2024-03-11 16:39:00 +0300

    Done

--- solution.py
+++ solution.py
@@ -2,3 +2,14 @@ def solution(A, D):
   balance = 0
   mothly_payment = 0
   totoal_income = 0
+  
+  for i in range(len(A)):
+      if A[i] < 0:
+          month_payments += 1
+          month_total += A[i]
+      else:
+          month_payments = 0
+          month_total = 0
+      balance += A[i]
+        
+        
